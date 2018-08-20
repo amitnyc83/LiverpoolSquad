bec2cdfc825abd4a1a499c325f9b571ec3c20b33
Amit Singh <amit_nyc@live.com>
2018-08-20 19:08:26 +0000

    remove double enviroment files

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-
-require_relative '../lib/premier_league_teams/cli'
-require_relative '../lib/premier_league_teams/player'
-require_relative '../lib/premier_league_teams/scraper'
-require_relative '../lib/premier_league_teams/version'
